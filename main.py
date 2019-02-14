eee2c28c76bce28de1ba6dac6e1e5802d31b9e82
Nikolai Kiselev <31371501+nshrd@users.noreply.github.com>
2019-02-14 19:35:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import os
 import argparse
@@ -9,24 +10,24 @@ def main():
     load_dotenv()
     token = os.getenv('TOKEN')
     link = get_arguments()
-    is_bitlink = get_link_form(token, link)
-    if not is_bitlink :
+    is_bitlink = check_if_link_is_bitlink(token, link)
+    if not is_bitlink:
         new_bitlink = get_bit_link(token, link)
         if new_bitlink is None:
-            print('Ссылка не найдена либо введена с ошибкой.')
+            print('Link can\'t be found or you did type mistake.')
         else:
-            print(f'Ваш битлинк: {new_bitlink}')
+            print(f'Your bitlink: {new_bitlink}')
     elif is_bitlink:
         total_clicks = get_clicks_info(token, link)
         if total_clicks is None:
-            print('Ссылка не найдена либо введена с ошибкой.')
+            print('Link can\'t be found or you did type mistake.')
         else:
-            print(f'Количество переходов: {total_clicks}')
+            print(f'Total clicks: {total_clicks}')
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("link", help="if your link is bit.ly link - you will see total clicks statistics,\
+    parser.add_argument('link', help="if your link is bit.ly link - you will see total clicks statistics,\
                             otherwise you will get new bit.ly link")
     args = parser.parse_args()
     return args.link
@@ -37,7 +38,7 @@ def get_bit_link(token, link):
     header_query = {'Authorization': f'Bearer {token}'}
     payload = {'long_url': f'{link}'}
     response = requests.post(url, json=payload, headers=header_query)
-    if not response.ok :
+    if not response.ok:
         return
     decoded_resp = response.json()
     bitlink = decoded_resp['link']
@@ -45,8 +46,8 @@ def get_bit_link(token, link):
 
 
 def get_clicks_info(token, link):
-    link = urlparse(link)
-    url = f'https://api-ssl.bitly.com/v4/bitlinks/{link.netloc}{link.path}/clicks/summary'
+    parsed_link = urlparse(link)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_link.netloc}{parsed_link.path}/clicks/summary'
     header_query = {'Authorization': f'Bearer {token}'}
     payload = {'units': '-1'}
     response = requests.get(url, params=payload, headers=header_query)
@@ -57,9 +58,9 @@ def get_clicks_info(token, link):
     return total_clicks
 
 
-def get_link_form(token, link):
-    link = urlparse(link)
-    url = f'https://api-ssl.bitly.com/v4/bitlinks/{link.netloc}{link.path}'
+def check_if_link_is_bitlink(token, link):
+    parsed_link = urlparse(link)
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_link.netloc}{parsed_link.path}'
     header_query = {'Authorization': f'Bearer {token}'}
     response = requests.get(url, headers=header_query)
     return response.ok
